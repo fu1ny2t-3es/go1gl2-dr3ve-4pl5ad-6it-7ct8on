28204fb14d0bdb3d0129b547c19e473364d1b858
li1ht2ay <108039498+li1ht2ay@users.noreply.github.com>
2024-04-15 21:40:23 -0400

    Update main.go

--- main.go
+++ main.go
@@ -78,7 +78,7 @@ func main() {
 	fmt.Println("Files:")
 	if len(r.Files) != 0 {
 		for _, i := range r.Files {
-			fmt.Printf("%v (%vs)\n", i.Name, i.Id)
+			fmt.Printf("%v (%v)\n", i.Name, i.Id)
 		}
 	}
 
